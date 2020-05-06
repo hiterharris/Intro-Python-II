e10197236c9a3586bd52a5a658d49732d5f031fc
Hiter Harris <hiterharris@comcast.net>
2020-05-06 19:25:16 -0400

    updated room

--- src/room.py
+++ src/room.py
@@ -2,7 +2,7 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name="default name", description="default description", n_to=None, e_to=None, s_to=None, w_to=None, inventory=None):
+    def __init__(self, name, description, n_to=None, e_to=None, s_to=None, w_to=None, inventory=None):
         self.name = name
         self.description = description
         self.n_to = n_to
@@ -12,7 +12,7 @@ class Room:
 
     def __str__(self):
         return (self.name + ": " + self.description)
-        
+
     def get_next_room_for_direction(self, direction):
         if hasattr(self, f'{direction}_to'):
             return getattr(self, f'{direction}_to')
